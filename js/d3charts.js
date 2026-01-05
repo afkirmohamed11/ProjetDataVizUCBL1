dfd5f093c36a1c6ef3d62b71d10d86e50976fcbc
hamzaae <motassimhamza99@gmail.com>
2026-01-05 23:03:02 +0100

    init chart file

---
+++ js/d3charts.js
@@ -0,0 +1,21 @@
+// this file contains code from the D3.js library
+
+// Section 1 (Motassim): Why It Matters: Data in Our Daily Lives
+
+
+// Section 2 (Ben Touhami): Inside Your Device: How Much Energy Does It Use?
+
+
+// Section 3 (): When Data Leaves Your Device: Network & Cloud
+
+
+// Section 4 (Afkir): Servers & Data Centers: The Engines Behind the Internet (Californie & Portugal) 
+
+
+// Section 5 (Akkouh): Big Picture: Data Center Efficiency & Its Limits 
+
+
+// Section 6 (Motassim): UCBL1 Data Center: A Case Study
+
+
+// Section 7 (): What We Can Do: Solutions & Future Roadmap
